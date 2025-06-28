f40a360eae0f516d7e978527a46f5d0177336c81
StreetGame23 <sergioramero23@gmail.con>
2025-06-28 16:28:41 +0300

    Добавление скрипта отправки формы.

--- js/utils.js
+++
@@ -1,5 +0,0 @@
-const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-
-const isEscapeKey = (evt) => evt.key === 'Escape';
-
-export {getRandomInteger, isEscapeKey};
